6e0651deffcfed4c3d21c5ce1b154fd02e0d6d0b
MladenStankov <mladen.stankov17@gmail.com>
2024-09-08 14:36:43 +0300

    Created models for: User, Property, Favourite, Image; Connected with the database and created the tables

---
+++ backend/src/database/models/favourite.model.ts
@@ -0,0 +1,37 @@
+import { DataTypes, Model, InferAttributes, InferCreationAttributes, CreationOptional } from "@sequelize/core"
+import { Attribute, PrimaryKey, AutoIncrement, NotNull, Default, CreatedAt, UpdatedAt, Table } from "@sequelize/core/decorators-legacy"
+
+@Table({
+    indexes: [
+        {
+            unique: true,
+            fields: ['userId', 'propertyId']
+        }
+    ]
+})
+export default class Favourite extends Model<InferAttributes<Favourite>, InferCreationAttributes<Favourite>> {
+    @Attribute(DataTypes.INTEGER)
+    @PrimaryKey
+    @AutoIncrement
+    declare id: CreationOptional<number>
+
+    @Attribute(DataTypes.DATE)
+    @CreatedAt
+    @NotNull
+    @Default(DataTypes.NOW)
+    declare createdAt: CreationOptional<Date>
+
+    @Attribute(DataTypes.DATE)
+    @UpdatedAt
+    @NotNull
+    @Default(DataTypes.NOW)
+    declare updatedAt: CreationOptional<Date>
+
+    @Attribute(DataTypes.INTEGER)
+    @NotNull
+    declare userId: number
+
+    @Attribute(DataTypes.INTEGER)
+    @NotNull
+    declare propertyId: number
+}
