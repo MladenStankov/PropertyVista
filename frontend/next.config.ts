beb5251640fa91b98db0dc1079eba7625451f6c4
MladenStankov <mladen.stankov17@gmail.com>
2025-01-29 23:35:38 +0200

    cross origin in frontend?

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -12,29 +12,7 @@ const nextConfig: NextConfig = {
   },
   pageExtensions: ["ts", "tsx"],
   typescript: {},
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: String(process.env.NEXT_PUBLIC_API_URL),
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  crossOrigin: "use-credentials",
 };
 
 export default nextConfig;
